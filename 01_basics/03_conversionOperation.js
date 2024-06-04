cc21eb9e198786a8099aa0e3401dc8ca076e6600
damandeep611 <damandeep611@gmail.com>
2024-06-04 16:30:48 +0530

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = 33;
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
