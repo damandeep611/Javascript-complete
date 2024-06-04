e72b128f87b261363b729db40add998ba5781396
damandeep611 <damandeep611@gmail.com>
2024-06-04 14:56:23 +0530

    data types

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,1 +1,16 @@
-const accountId = "hello";
+const accountId = 144567;
+let accountEmail = "martian@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+// accountId = 2; this is not allowed
+accountEmail = "dk@dk.com";
+accountPassword = "21212112";
+accountCity = "Bengaluru";
+let accountState;
+
+/*
+preferred not to use to declare varialbes because of issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId, accountEmail, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // this will treat all JS code as newer version
+
+// alert(3 + 3); //we are using nodejs, not browser
+
+console.log(3 + 3);
+// code readability should be high
+
+let name = "daman";
+let age = 20;
+let isLoggedIn = true;
+let state = null;
+
+// data types
+// number
+// bigint => this is used for big number i.e stock market trading etc
+// string  => " "
+// boolean => true/false
+//null => standalone value
+// undefined => value is not assigned yet
+// symbol => used in react for making components i.e unique components
+
+// object =>
+
+console.log(typeof state);
