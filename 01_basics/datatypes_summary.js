52588715cb52aa67e16c5c3f9530a850de02b32a
damandeep611 <damandeep611@gmail.com>
2024-06-06 16:20:36 +0530

    data types revise

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,29 @@
+// prmitive Data types
+
+// there are 7 primitve data types :- 1. String 2. Number 3. Boolean 4. Null 5. undefined 6. Symbol 7. BigInt
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id === anotherId);
+
+const bigNumber = 343241351512434545n;
+
+// Reference data types or non-primitive data types -
+// Arrays, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga", "bongo"];
+
+let myObj = {
+  name: "martian",
+  age: 22,
+  work: "hitman",
+};
+
+const myFunction = function () {
+  console.log("hey Baby");
+};
+
+// how to find data type of variable
+console.log(typeof bigNumber);
+console.log(typeof myFunction);
